f9fa9703f0fa67f51cfb1984ba5c535efce28681
PrangonGhose <prangonghose@gmail.com>
2023-05-10 03:35:37 +0600

    Add necessary config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get "/reservations/:username", to: 'reservations#index'
+
   resources :houses, only: %i[index show create destroy]
   resources :reservations, only: %i[index create]
   resources :users, only: %i[index create]
