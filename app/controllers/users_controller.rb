fd9b5dc7c92c047e7edfd40d993bb33ce17339e9
PrangonGhose <prangonghose@gmail.com>
2023-05-10 03:55:10 +0600

    Render user when created else error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: :created
+    @user = User.new(user_params)
+    if @user.save
+      render json: { user: @user, status: :created }
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
